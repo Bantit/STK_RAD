907e38eae1ead3270070f3a86b646baf8b1bc92c
Bantit <Tw1819@msstate.edu>
2023-12-08 11:55:37 -0600

    Update .3
    Sorry for the lack of updates, but we have gotten past the exam phase.
    
    This update is the update that includes all of the restaurants in Starkville, MS!
    
    Still going through and applying tags the restaurants to make specific types of food.
    
    But hopefully now that I have gotten past some classes and the winter break would allow me to punch out a lot of this.

--- res_push.js
+++ res_push.js
@@ -1,36 +1,552 @@
 var restaurantData = [
     {
-        name: "Italian Restaurant 1",
-        category: ["Italian", "Cheap"],
-        info: "Information about Italian Restaurant 1"
+        name: "Walk-ons",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
     },
     {
-        name: "Italian Restaurant 2",
-        category: "Italian",
-        info: "Information about Italian Restaurant 2"
+        name: "The Breakfast Club",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
     },
     {
-        name: "Downtown Restaurant 1",
-        category: "Downtown",
-        info: "Information about Downtown Restaurant 1"
+        name: "Steak ‘n Shakes",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
     },
     {
-        name: "Downtown Restaurant 2",
-        category:["Downtown", "Cheap"],
-        info: "Information about Downtown Restaurant 2"
+        name: "Moes southwest grill",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
     },
     {
-        name: "Local Restaurant 1",
-        category: "Locals",
-        info: "Information about Local Restaurant 1"
+        name: "Chick-Fil-A Union",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
     },
     {
-        name: "Local Restaurant 2",
-        category: "Locals",
-        info: "Information about Local Restaurant 2"
+        name: "Panda Express Union",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Newk’s Union",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Subway Union",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Einsteins Bros Bagels Library",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Staggerin Sports Grill",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Boardtown Pizza and Pints",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Arepas Coffee & Bar",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "The Klaasroom",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "NutriGroove",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Mojo Maroons",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Bulldog Burger",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Bin 612",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Two Brothers",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Snack Attack",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Uno Mas Tacos",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Mugshots",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Papa Johns",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "McAlister’s Deli",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Buffalo Wild Wings",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Jimmy John’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Hungry Howes",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Salsarita’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Chick-fil-a",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Churn and spoon",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Insomnia Cookies",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Strange Brew",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Little Caesars",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Starbucks",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Taste Italian Eatery",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Commodores Bob’s Yacht Club",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Bluto’s Greek Tavern",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Dolce Gelato Treats",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "L’uva Wine Room",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Little Dooey’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Andaman Thai",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Hardee’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Burger King",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Harveys",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Shipleys",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Subways",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "The Camphouse",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Dave Dark Horse Tavern",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Tienda Hispana Las Aguilar",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Godfather’s Pizza",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Thai Siam Restaurant",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Central Station Grill",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Slim Chickens",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Sonic Drive-in",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Georgia Blue",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "44-Prime",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "The Coffee Depot",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Domino’s Pizza",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Chipotle Mexican Grill",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Wendys",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Rosey Baby",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Starkville Café",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Hotel Chester ?",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Cook Out",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Oby’s of Starkville",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Dairy Queen",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Casa Bravo Mexican Restaurant",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Newk’s Eatery",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "No way Jose",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "La Terraza",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "KFC",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Taco Bell",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "The Joint",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Bop’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Moe’s Original BBQ – Starkville",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Nine-Twentynine coffee bar",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Upstairs at Tyler",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "The Guest Room",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Pita Pit",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Proof",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Flavors Cuisine of India",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "WTF @ Starkville",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Asian foods Market-Restaurant",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Guadalajara",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "The Spotlight Café",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Smoothie King",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Tyummi Café & Pho Bowl",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Lost Pizza Co Starkville",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Brewski’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Umi Japanese Steakhouse",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Firehouse Subs",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Jack’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Umble",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Wingstop",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Wing express café",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Applebee’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "McDonald’s West",
+        category: "Master",
+        info: "infor is being Updated in the next update, hopfully"
+    },
+    {
+        name: "China Garden Restaurant",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Popeyes Louisiana Kitchen",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Zaxby’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Jersey Mike’s",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "El Veracruz",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Sweet Peppers Deli",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Mi Hacienda Mexican Restaurant",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "OEC Japanese Express",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Pizza Hut",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Subway",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Sonic Drive-in West",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Panda Express",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
+    },
+    {
+        name: "Waffle House",
+        category: "Master",
+        info: "Info is being Updated in the next update, hopefully"
     }
-    // Add more restaurant objects as needed
 ];
+
+    
 
 function pickRandomRestaurant() {
     var category = document.getElementById("restaurantCategory").value;
